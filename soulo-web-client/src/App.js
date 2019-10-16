52b4cd7900c82d97825c166b0435e10fd1cfc7eb
ZanifSandy <zanif.sandy@student.mec.cuny.edu>
2019-10-15 21:35:54 -0400

    Content...

--- soulo-web-client/src/App.js
+++ soulo-web-client/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from "react-router-dom";
+import "./App.css";
 
-function App() {
+import greetingPage from "./pages/greetingPage";
+import loginPage from "./pages/loginPage";
+
+function Navigation(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark shadow mb-3">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item">
+          <NavLink className="navbar-brand nav-link2 " exact to="/">
+            <img alt="soulo" className="App-logo" src={require("./logo.png")} />
+          </NavLink>
+        </li>
+
+        <li className="nav-login">
+          <NavLink className="nav-link" exact to="/login">
+            login/signup
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
   );
+}
+
+function Footer(props) {
+  return <div className="footer">copyright © soulo 2019</div>;
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Navigation />
+        <div className="container-fluid text-center">
+          <div className="row justify-content-center">
+            <Switch>
+              {/* <Route path="/posts/:id" component={ShowPostPage} /> */}
+              {/* <Route path="/about-us" component={AboutUsPage} /> */}
+              <Route path="/login" component={loginPage} />
+              <Route path="/" component={greetingPage} />
+            </Switch>
+          </div>
+        </div>
+        <Footer />
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ soulo-web-client/src/pages/loginPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function loginPage(props) {
+  return <div className="Container">hello</div>;
+}
+
+export default loginPage;
