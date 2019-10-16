e5cc101fe4823f81567e163a378db1e0e44a4307
ZanifSandy <zanif.sandy@student.mec.cuny.edu>
2019-10-16 00:46:33 -0400

    content..

--- soulo-web-client/src/pages/loginPage.js
+++ soulo-web-client/src/pages/loginPage.js
@@ -1,7 +1,33 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
+import useForm from "react-hook-form";
+import "./loginPage.css";
 
+//I used a hook to save the user Form Validation imported from react-hook-form
+//Data returns an Object. obj = {Username : "text", Password : "ptext"}
 function loginPage(props) {
-  return <div className="Container">hello</div>;
+  const { register, handleSubmit, errors } = useForm(); // initialise the hook
+  const onSubmit = data => {
+    console.log(data);
+  };
+
+  return (
+    <div className="Container">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label for="fname">Username</label>
+        <input className="userField" name="Username" ref={register} />
+        <label for="fname">Password</label>
+        <input
+          className="userField"
+          name="Password"
+          type="password"
+          ref={register({ required: true })}
+        />
+        {errors.Password && "Password is required."}
+        <input type="submit" name="login" />
+      </form>
+    </div>
+  );
 }
 
 export default loginPage;
